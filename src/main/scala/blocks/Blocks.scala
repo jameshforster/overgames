5b7057711be51756816409f78c8d246f6803b6a9
James Forster <jameshforster@users.noreply.github.com>
2019-05-30 20:37:33 +0100

    Initial commit.

---
+++ src/main/scala/blocks/Blocks.scala
@@ -0,0 +1,13 @@
+package blocks
+
+import net.minecraft.block.Block
+import net.minecraft.block.material.Material
+
+object Blocks {
+
+  val allModBlocks: Seq[Block] = Seq()
+
+  class TestBlock extends Block(Material.GROUND) {
+
+  }
+}
---
+++ src/main/scala/blocks/TestBlock.scala
@@ -0,0 +1,5 @@
+package blocks
+
+class TestBlock {
+
+}
